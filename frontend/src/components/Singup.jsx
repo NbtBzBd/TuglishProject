ec294a186505cf3d204bf77971e34697efac51f7
Nalberth <fredbearolden@gmail.com>
2025-02-11 13:25:11 -0300

    teste

--- frontend/src/components/Singup.jsx
+++ frontend/src/components/Singup.jsx
@@ -44,6 +44,8 @@ export default function() {
             passwordValidation();
             comfirmValidation();
         }
+
+        console.log()
         
     }
     function showError(index) {
